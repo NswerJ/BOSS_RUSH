e10b9d4994faf63a397f2ed46824f1bb30a41c73
dkdkdsa <bos94330@gmail.com>
2024-01-02 22:20:11 +0900

    HitFeedbackPlayer

---
+++ Assets/02_Script/UI/HP/PlayerHPBar.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHPBar : MonoBehaviour
+{
+
+    [SerializeField] private Slider hpBarSlider;
+
+    private HitObject hitObject;
+
+}
