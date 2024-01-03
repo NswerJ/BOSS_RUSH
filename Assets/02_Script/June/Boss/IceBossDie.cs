8258d8d45ee08b4d5dbbd4789f7cd2c94ec8c7ed
준1 <98933515+Wizone06@users.noreply.github.com>
2024-01-04 00:41:58 +0900

    대쉬 사운드 바꿈
    대쉬 사운드 바꿈

--- Assets/02_Script/June/Boss/IceBossDie.cs
+++ Assets/02_Script/June/Boss/IceBossDie.cs
@@ -5,12 +5,9 @@ using UnityEngine;
 public class IceBossDie : BossDieCheck
 {
     [SerializeField] GameObject PowerUpDoor;
-    public float HiddenTime = 0;
+    private float HiddenTime = 0;
 
-    private void Start()
-    {
-        HiddenTime = 0;
-    }
+    
 
     public override void DieEvent()
     {
@@ -22,7 +19,7 @@ public class IceBossDie : BossDieCheck
         Invoke("EndFun", 1.65f);
     }
 
-    private void Update()
+    private void LateUpdate()
     {
         HiddenTime += Time.deltaTime;
     }
