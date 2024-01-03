6c5dab951a6f62e26819d93a0e1fcbb7decc1c1a
dkdkdsa <bos94330@gmail.com>
2024-01-03 19:05:19 +0900

    asdfasdfasdfasdfasdfasdf

--- Assets/02_Script/Boss/Ice/IceAwakeCutScene.cs
+++ Assets/02_Script/Boss/Ice/IceAwakeCutScene.cs
@@ -4,16 +4,22 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class IceAwakeCutScene : MonoBehaviour
 {
 
     [SerializeField] private Image fadeImage;
-    [SerializeField] private TMP_Text text;
+    [SerializeField] private Image blinkImage;
+    [SerializeField] private TMP_Text bossText;
+    [SerializeField] private UnityEvent blinkEvt;
+    [SerializeField] private UnityEvent endEvent;
 
     private void Start()
     {
+
+        bossText.color = new Color(0, 0, 1, 0);
 
         fadeImage.DOFade(1, 0);
         fadeImage.DOFade(0, 0.5f).OnComplete(() =>
@@ -28,7 +34,42 @@ public class IceAwakeCutScene : MonoBehaviour
     private void FadeEnd()
     {
 
+        StartCoroutine(TextShowing());
 
+    }
+
+    private void TextShow(string text)
+    {
+
+        bossText.text = text;
+
+        Sequence seq = DOTween.Sequence();
+
+        seq.Append(bossText.DOFade(1, 1f).SetEase(Ease.OutQuart));
+        seq.AppendInterval(0.1f);
+        seq.Append(bossText.DOFade(0, 1f).SetEase(Ease.InSine));
+
+    }
+
+    private IEnumerator TextShowing()
+    {
+
+        yield return new WaitForSeconds(0.5f);
+
+        TextShow("나를 여기까지 밀어 붙이다니");
+        yield return new WaitForSeconds(2.7f);
+        TextShow("지금부터 진심으로 상대해주지");
+        yield return new WaitForSeconds(2.7f);
+
+        blinkImage.gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        blinkEvt?.Invoke();
+        blinkImage.gameObject.SetActive(false);
+
+        yield return new WaitForSeconds(1.5f);
+        endEvent?.Invoke();
+
+        //Blink
 
     }
 
