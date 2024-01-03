e3f23446a2a4302d455696c7543005cd1385cbf5
준이 <98876187+June060522@users.noreply.github.com>
2024-01-03 14:58:02 +0900

    DataSave

--- Assets/02_Script/Core/DataManager.cs
+++ Assets/02_Script/Core/DataManager.cs
@@ -10,6 +10,9 @@ public class DataManager : DontDestroyOnLoad
     public int DataIndex => _dataIndex;
 
     int bossCnt;
+    public int BossCount => bossCnt;
+
+    Dictionary<string, int> clearDic = new Dictionary<string, int>();
 
     [SerializeField] private GameObject _settingPanel;
 
@@ -34,11 +37,22 @@ public class DataManager : DontDestroyOnLoad
         bossCnt = FindObjectsOfType<StageDoor>().Length;
     }
 
+    private void Start()
+    {
+        for (int i = 0; i < BossCount; i++)
+        {
+            string s = "Boss" + i + 1;
+            string s1 = "File" + DataIndex;
+            clearDic.Add(s, PlayerPrefs.GetInt(s1 + s, 0));
+        }
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Time.timeScale = 0f;
+            PlayerPosSave.Instance.SavePos();
             _settingPanel.SetActive(true);
         }
     }
@@ -48,11 +62,13 @@ public class DataManager : DontDestroyOnLoad
         Time.timeScale = 1f;
         _settingPanel.SetActive(false);
     }
+
     public void Main()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("IntroScene");
     }
+    
     public void InitAndMain()
     {
         InitData();
@@ -69,6 +85,29 @@ public class DataManager : DontDestroyOnLoad
         PlayerPrefs.SetInt("File" + _dataIndex, -1);
         PlayerPrefs.SetFloat("File" + _dataIndex + "XPos", -5);
         PlayerPrefs.SetFloat("File" + _dataIndex + "YPos", 0);
+        for(int i = 0; i < bossCnt; i++)
+            PlayerPrefs.SetInt("File" + _dataIndex + "Boss" + (i + 1), 0);
     }
 
+    bool GetClear(string key)
+    {
+        if (!clearDic.ContainsKey(key))
+            Debug.LogError("Can not find Key");
+        return clearDic[key] == 1;
+    }
+
+    bool GetClear(int key)
+    {
+        string keyStr = "Boss" + key;
+        if (!clearDic.ContainsKey(keyStr))
+            Debug.LogError("Can not find Key");
+        return clearDic[keyStr] == 1;
+    }
+
+    void ClearMap(int key)
+    {
+        string keyStr = "Boss" + key;
+        clearDic[keyStr] = 1;
+        PlayerPrefs.SetInt("File" + DataIndex + "Boss" + key, 1);
+    }
 }
