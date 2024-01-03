d7e9cb5fd2e35595cdbdad5d22aa4c9af63de761
dkdkdsa <bos94330@gmail.com>
2024-01-03 14:17:02 +0900

    asdfasdf

--- Assets/02_Script/Boss/Ice/IceAwakeningController.cs
+++ Assets/02_Script/Boss/Ice/IceAwakeningController.cs
@@ -7,11 +7,14 @@ public enum EnumIceAwakeState
 {
 
     Pattern_1, //상단부 이동후 일직선으로 샤드 날림
+    Pattern_2, //뫼비우스띠 모양으로 이동하며 일정 시간마다 플레이어 방향으로 샤드 날림 && 카메라 주도권이 보스에게 넘어감
 
 }
 
 public class IceAwakeningController : FSM_Controller<EnumIceAwakeState>
 {
+
+    [SerializeField] private EnumIceAwakeState startState;
 
     protected override void Awake()
     {
@@ -19,7 +22,10 @@ public class IceAwakeningController : FSM_Controller<EnumIceAwakeState>
         var p1 = new Ice_Pattern_1_State(this);
         AddState(p1, EnumIceAwakeState.Pattern_1);
 
-        ChangeState(EnumIceAwakeState.Pattern_1);
+        var p2 = new Ice_Pattern_2_State(this);
+        AddState(p2 , EnumIceAwakeState.Pattern_2);
+
+        ChangeState(startState);
 
     }
 
