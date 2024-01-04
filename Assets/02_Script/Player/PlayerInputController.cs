e5381f0bfe67c109561303120bace6684c0db42b
dkdkdsa <bos94330@gmail.com>
2024-01-04 19:47:19 +0900

    Update PlayerInputController.cs

--- Assets/02_Script/Player/PlayerInputController.cs
+++ Assets/02_Script/Player/PlayerInputController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerInputController : IDisposable
 {
@@ -18,6 +19,51 @@ public class PlayerInputController : IDisposable
         SettingMove();
         JumpKeyPress();
         DashKeyPress();
+
+        #region Debug
+
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+
+            SceneManager.LoadScene("IceEnd");
+
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha9)) 
+        {
+
+            var obj = UnityEngine.Object.FindObjectsOfType<HitObject>();
+
+            foreach(var item in obj)
+            {
+
+                if (item.name == "Player") continue;
+
+
+                item.defecnces.AddMod(-1000);
+            }
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+
+            var obj = UnityEngine.Object.FindObjectsOfType<HitObject>();
+
+            foreach (var item in obj)
+            {
+
+                if (item.name == "Player")
+                {
+
+                    item.HealingObject(1000);
+
+                }
+
+            }
+
+
+        }
+
+        #endregion
 
     }
 
