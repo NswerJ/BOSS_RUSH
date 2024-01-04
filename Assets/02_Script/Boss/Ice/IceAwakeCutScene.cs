e18493773e8b9f176b7fae970acb8b207e6b2b3a
dkdkdsa <bos94330@gmail.com>
2024-01-04 14:51:32 +0900

    ㅁㄴㅇㄻㄴㅇㄹ

--- Assets/02_Script/Boss/Ice/IceAwakeCutScene.cs
+++ Assets/02_Script/Boss/Ice/IceAwakeCutScene.cs
@@ -15,6 +15,7 @@ public class IceAwakeCutScene : MonoBehaviour
     [SerializeField] private TMP_Text bossText;
     [SerializeField] private UnityEvent blinkEvt;
     [SerializeField] private UnityEvent endEvent;
+    [SerializeField] private AudioClip ding, chgit;
 
     private void Start()
     {
@@ -57,12 +58,15 @@ public class IceAwakeCutScene : MonoBehaviour
         yield return new WaitForSeconds(0.5f);
 
         TextShow("나를 여기까지 밀어 붙이다니");
+        SoundManager.Instance.SFXPlay("ding", ding);
         yield return new WaitForSeconds(2.7f);
         TextShow("지금부터 진심으로 상대해주지");
+        SoundManager.Instance.SFXPlay("ding", ding);
         yield return new WaitForSeconds(2.7f);
 
         blinkImage.gameObject.SetActive(true);
         yield return new WaitForSeconds(0.1f);
+        SoundManager.Instance.SFXPlay("chgit", chgit);
         blinkEvt?.Invoke();
         blinkImage.gameObject.SetActive(false);
 
